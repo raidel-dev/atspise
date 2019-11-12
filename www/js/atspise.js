05ee87b52bb899f7e2d6ffec657470b24e4114f0
dalba1992 <dalba1992@yandex.com>
2019-11-12 12:43:49 +0800

    updates

--- www/js/atspise.js
+++ www/js/atspise.js
@@ -1,18 +1,38 @@
-		var place = "Vadodara";
-				
-		$(document).ready(function(){
-			alert("Works !");
-			$.ajax({
-        	 url: 'http://www.pontun.online/atspise_home.php?release=1',
-        	 cache: false,
-            success: function (response) {
-				alert("Sucess");
-				$('#app_home').append(response);			
-			},
-			error: function(XHR){
-				alert(XHR.status);
-			}
-			
-          });
-		  
-		}); 
+var place = "Vadodara";
+
+$(document).ready(function () {
+	alert("Works !");
+
+	$.ajax({
+		url: 'https://www.pontun.online/atspise_home.php?release=1',
+		success: function (response) {
+			alert("Sucess");
+			navigator.geolocation.getCurrentPosition(
+				onSuccess, onError, { timeout: 1000, enableHighAccuracy: true }
+			)
+			$('#app_home').append(response);
+		},
+		error: function (XHR) {
+			alert(XHR.status);
+		}
+
+	});
+
+});
+var onSuccess = function (position) {
+	alert('Lat: ' + position.coords.latitude + '\n' +
+		'Lng: ' + position.coords.longitude + '\n');
+}
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+	alert('code: ' + error.code + '\n' +
+		'message: ' + error.message + '\n');
+}
+
+
+
+
+
+
